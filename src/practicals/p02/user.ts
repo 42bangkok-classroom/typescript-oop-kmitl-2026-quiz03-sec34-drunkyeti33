8fa1e3f2aaa086a7f988b70dc2bbcccb7856a7ee
Wiphusan Choonpicharn <itancomemaiback@gmail.com>
2026-02-20 17:44:29 +0700

    2

--- src/practicals/p02/user.ts
+++ src/practicals/p02/user.ts
@@ -1,1 +1,23 @@
-export class User {}
+export class User {
+    firstname?:string
+    lastname?:string
+    private age?:number
+    BIRTH_YEAR: number
+    
+    setFirstname(firstname:string){
+        return this.firstname = firstname
+    }
+setLastname(lastname:string){
+    return this.lastname = lastname
+}
+setAge(age:number){
+    return this.age = age
+}
+getAge(){
+    return this.age
+}
+BIRTH_YEAR = 2025 - this.age
+getFullName(){
+    return this.firstname + ' ' + this.lastname
+}
+}
