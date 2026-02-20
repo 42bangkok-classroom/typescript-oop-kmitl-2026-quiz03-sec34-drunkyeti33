46b8e9957165f3e87db57a9d5e11633bc3220c90
Wiphusan Choonpicharn <itancomemaiback@gmail.com>
2026-02-20 18:11:27 +0700

    7

--- src/practicals/p03/warrior.ts
+++ src/practicals/p03/warrior.ts
@@ -1,5 +1,20 @@
 import { Character } from "./character";
 
 export class Warrior extends Character {
+  name?:string
+  weapon?:string
+  health:number
+  constructor(name:string,weapon:string,health:number){
+    super()
+    this.name = name
+    this.weapon = weapon
+    this.health = health
+  }
+  getWeapon(){
+    return this.weapon}
+
+    receiveDamage(damage: number){
+    return this.health = this.health - damage*0.9
+  }
   // TODO: implement class properties, constructor with super(...), and methods
 }
