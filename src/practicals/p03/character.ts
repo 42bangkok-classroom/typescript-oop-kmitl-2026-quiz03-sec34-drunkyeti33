2e1435f8c4f22ff789019fd69900e352ca26cefe
Wiphusan Choonpicharn <itancomemaiback@gmail.com>
2026-02-20 19:28:27 +0700

    15

--- src/practicals/p03/character.ts
+++ src/practicals/p03/character.ts
@@ -1,1 +1,13 @@
-export class Character {}
+export class Character {
+    name?:string
+    health:number = 100
+    getName(){
+        return this.name
+    }
+    receiveDamage(damage: number){
+        return this.health = this.health - damage
+    }
+    getHealth(){
+        return this.health
+    }
+}
